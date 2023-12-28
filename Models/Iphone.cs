1a0e68a0f20f628e02d5e2b1eb372808100a664c
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-28 14:33:45 -0300

    feat: creating child classes

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace PooDesafioDio.Models
 {
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"O aplicativo {nome} foi instalado com sucesso no meu Iphone {Modelo}.");
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace PooDesafioDio.Models
 {
-    public class Nokia
+    public class Nokia: Smartphone
     {
-        
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine($"O aplicativo {nome} foi instalado com sucesso no meu Nokia {Modelo}.");
+        }
     }
 }
