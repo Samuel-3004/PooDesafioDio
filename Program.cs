45b5c44ae0e164db7359c68e6358bf1cd5d8fceb
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-28 14:34:50 -0300

    test: functionalities

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace PooDesafioDio
+﻿using PooDesafioDio.Models;
+
+namespace PooDesafioDio
 {
   class Program
   {
     static void Main(String[] args)
     {
+      Console.WriteLine("Smartphone: Iphone");
+      Iphone smApple = new Iphone(numero: "123456789", modelo: "13s", imei: "454545", memoria: 48 );
+      smApple.Ligar();
+      smApple.ReceberLigacao();
+      smApple.InstalarAplicativo(nome: "WhatsApp");
+
+      Console.WriteLine("");
       
+      Console.WriteLine("Smartphone: Nokia");
+      Nokia smNokia = new Nokia(numero: "987654321", modelo: "Novo", imei: "#5454548", memoria: 10);
+      smNokia.Ligar();
+      smNokia.ReceberLigacao();
+      smNokia.InstalarAplicativo(nome: "Facebook");
+    
     }
   }
 }
